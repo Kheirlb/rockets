9e1663a9560bd15dfc3212225b47d1958856680f
Karl Parks <kheirlbparks@gmail.com>
2019-02-15 14:33:24 -0800

    New Changes - Altitude

--- rocketSimMainV1.m
+++ rocketSimMainV1.m
@@ -2,14 +2,17 @@
 % Author: Karl Parks
 % Date: 2.13.19
 
+%% Given
 angle = 1; %deg
 Mo = 750; %kg
 Ml = 10;
 Ms = 240;
-Mp = Mo - Ml - Ms;
-t = 60; %sec
+Mb = Ml + Ms;
+Mp = Mo - Mb;
+burntime = 60; %sec
 thrust = 20000; %N
 frontArea = 0.196; %m^2
+g = 9.81;
 
 fileName = 'rocketSimExcel.xlsx';
 %col1 = Mach 0.01 increments
@@ -21,5 +24,32 @@ CdvsMach = xlsread(fileName, 1, 'A2:C2501');
 %col3 = Density [kg/m^3]
 atmosphereData = xlsread(fileName, 2, 'A3:C1203');
 
+%% Calcs
+tStep = 0.1;%adjust
+tF = 100; %time final adjust
+tTotal = 1:tStep:tF;
+tPower = 1:tStep:burntime;
+
+mdot = Mp/burntime; 
+isp = thrust/(mdot*g);
+ueq = isp*g;
+R = Mo/Mb;
+deltaU = ueq*log(R); 
+count = 1;
+hPower = zeros(1, size(tPower, 2));
+
+for t = tPower 
+    hPower(count) = -ueq*t*((log(R))/(R-1)) + ueq*t - 0.5*g*(t^2);
+    count = count + 1;
+end
+hB = hPower(burntime);
+plot(tPower, hPower);
+
+%% Print Stuff
+fprintf("Total Weight %2.0f pounds\n", convmass(Mo,'kg','lbm'));
+fprintf("Thrust: %2.0f pounds\n", convforce(thrust,'N','lbf'));
+fprintf("T/W: %2.3f \n", thrust/(Mo*9.81));
+fprintf("Burnout Altitude: %2.0f meters\n", hB); 
+fprintf("Burnout Altitude: %2.0f feet\n", convlength(hB,'m','ft')); 
 
 
